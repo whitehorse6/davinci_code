146111aa41da4b1ed16c7f87e8bbda3d79ef250f
k <tjsrn0524@naver.com>
2017-10-02 09:26:46 +0900

    f/c

--- devleesk/app.js
+++ devleesk/app.js
@@ -4,15 +4,19 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+	, routes = require('./routes')
+	, chatting = require('./routes/chatting')
+	, user = require('./routes/user')
+	, http = require('http')
+	, path = require('path');
+
+const socket_io = require('socket.io');
 
 var app = express();
 
+
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3003);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -28,8 +32,47 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/chatting', chatting.chatting);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+
+const server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+const io = socket_io.listen(server);
+io.on('connection', (soc) => {
+	soc.on('message_to_server', (message, room_id) => {
+		io.sockets.in(room_id).emit('message_to_client', {
+			code : 200,
+			user_id : soc.id,
+			room_id : room_id,
+			result : "id[" + soc.id + "], massage - [" + message + "]"
+		});
+	});
+	soc.on('get_room', (rooms) => {
+		io.sockets.emit('room_to_client', io.sockets.manager.rooms);
+	});
+	soc.on('set_room', (room) => {
+		soc.join(room);
+		io.sockets.emit('room_to_client', io.sockets.manager.rooms);
+	});
+	soc.on('join_room', (room_id) =>{
+		
+		soc.join(room_id);
+		io.sockets.in(room_id).emit('access_room_check', {
+			code : 200,
+			user_id : soc.id,
+			room_id : room_id,
+			result : "success"
+		});
+		io.sockets.in(room_id).emit('message_to_client', {
+			code : 200,
+			user_id : soc.id,
+			room_id : room_id,
+			result : "id[" + soc.id + ", join room[" + room_id + "]"
+		});
+		console.log("rooms = " + io.sockets.manager.rooms[room_id])
+
+	});
+})
