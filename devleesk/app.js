3a901c01642707980d111a898c48f7ac2065790b
k <tjsrn0524@naver.com>
2017-10-02 10:06:44 +0900

    서버와 클라이언트 영역을 분리, server와 client로 나눔, server에는 서버에 대한 요청과 서버에서 구현할 비지니스 로직 부분 , [리퀘스트 요청 분리하여 controller, 서버의 비지니스 로직을 구현할 service] client에는 resources 를 관리, html 페이지 부분,

--- devleesk/app.js
+++ devleesk/app.js
@@ -4,27 +4,26 @@
  */
 
 var express = require('express')
-	, routes = require('./routes')
-	, chatting = require('./routes/chatting')
-	, user = require('./routes/user')
+	, routes = require('./server/controller')
+	, chatting = require('./server/controller/chatting')
+	, user = require('./server/controller/user')
 	, http = require('http')
-	, path = require('path');
-
-const socket_io = require('socket.io');
+	, path = require('path')
+	, chatting_manager = require('./server/service/chatting_manager');
 
 var app = express();
 
 
 // all environments
 app.set('port', process.env.PORT || 3003);
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/client/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/resources')));
 
 // development only
 if ('development' == app.get('env')) {
@@ -40,39 +39,4 @@ const server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-const io = socket_io.listen(server);
-io.on('connection', (soc) => {
-	soc.on('message_to_server', (message, room_id) => {
-		io.sockets.in(room_id).emit('message_to_client', {
-			code : 200,
-			user_id : soc.id,
-			room_id : room_id,
-			result : "id[" + soc.id + "], massage - [" + message + "]"
-		});
-	});
-	soc.on('get_room', (rooms) => {
-		io.sockets.emit('room_to_client', io.sockets.manager.rooms);
-	});
-	soc.on('set_room', (room) => {
-		soc.join(room);
-		io.sockets.emit('room_to_client', io.sockets.manager.rooms);
-	});
-	soc.on('join_room', (room_id) =>{
-		
-		soc.join(room_id);
-		io.sockets.in(room_id).emit('access_room_check', {
-			code : 200,
-			user_id : soc.id,
-			room_id : room_id,
-			result : "success"
-		});
-		io.sockets.in(room_id).emit('message_to_client', {
-			code : 200,
-			user_id : soc.id,
-			room_id : room_id,
-			result : "id[" + soc.id + ", join room[" + room_id + "]"
-		});
-		console.log("rooms = " + io.sockets.manager.rooms[room_id])
-
-	});
-})
+chatting_manager.chatting_manager(server);
